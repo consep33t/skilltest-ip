96e6cabc4553aa3b467b578ee84d0324e0484ba0
consep33t <129822994+consep33t@users.noreply.github.com>
2023-04-06 07:56:03 +0700

    Update index.js

--- backend test/logic_test/multiples/index.js
+++ backend test/logic_test/multiples/index.js
@@ -1,7 +1,4 @@
 function solution(n) {
-    /*
-    mengembalikan hasil penjumlahan semua bilangan yang merupakan kelipatan 3 atau 5 dan kurang dari n, serta bilangan-bilangan tersebut
-    */
     let sum = 0;
     let multiples = [];
     for (let i = 0; i < n; i++) {
