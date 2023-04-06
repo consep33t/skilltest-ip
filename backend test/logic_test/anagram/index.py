24bbe65f3700bed3535051bcb4c87e8ed073503e
consep33t <129822994+consep33t@users.noreply.github.com>
2023-04-06 07:51:36 +0700

    Update index.py

--- backend test/logic_test/anagram/index.py
+++ backend test/logic_test/anagram/index.py
@@ -1,3 +1,4 @@
+#disini saya membuat program menentukan apakah sebuah kalimat di katakan anagram atau tidak dengan membaca inputan dari user
 def is_anagram(str1, str2):
     str1 = str1.replace(" ", "").lower()
     str2 = str2.replace(" ", "").lower()
